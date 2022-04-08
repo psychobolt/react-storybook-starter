177b7e646d6b100a2e01cdf4df5ce9d8d986a14e
psychobolt <560721+psychobolt@users.noreply.github.com>
2022-04-08 15:36:09 -0700

    Update storybook config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.js"
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.js"
   ],
   "addons": [
     "@storybook/addon-links",
